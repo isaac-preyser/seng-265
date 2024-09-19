f6091c4d5c9c93995ac4cbf8c00f087e6c3bc0f4
Isaac Preyser <preyser@uvic.ca>
2024-09-19 12:32:46 -0700

    Initial prototype of getQuestions(), however I'm not sure if it's working. Further testing needed.

--- a1/survey.c
+++ a1/survey.c
@@ -32,11 +32,35 @@ int* getControlBits() {
     return control_bits;
 }
 
+char* getQuestions() {
+	//vscode and nano use different amount of whitespace for a tab. interesting...
+	static char* questions[200]; //200 questions max... should be enough, right?
+	char line[3000]; //hopefully 3000 characters is wide enough for one line. 
+	char *token; 
+	int line_number = 0; 
+
+	while(fgets(line, 3000, stdin) != (NULL)){
+		line_number++; //for debug purposes. 
+		//if the line starts with a #, skip it. 
+		if (line[0] == '#'){
+		continue;
+	}
+	//tokenize the questions. 
+	token = strtok(line, ";");
+	int i = 0; 
+	while (token != NULL){
+		//add the current token to the first avaliable space in the string array. 
+		strcpy(questions[i], token);
+		i++;
+	}
+
+}
+
 
 int main() {
 
     int* control_bits = getControlBits();
-
+    char* questions = getQuestions(); 
     // Print control bits for debugging
     printf("Control Bits: \n");
     for(int i = 0; i < 4; i++) {
