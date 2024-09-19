c01ceaaa3275220f8cebaf04a14d59fd593507c9
Isaac Preyser <preyser@uvic.ca>
2024-09-19 16:19:00 -0700

    implemented scanning for answer options, questions, etc.

--- a1/survey.c
+++ a1/survey.c
@@ -7,13 +7,19 @@ Date:    9/18/2024
 #include <string.h>
 #include <stdlib.h>
 
+const int MAX_LINE_LENGTH = 3000;
+const int MAX_QUESTION_AMOUNT = 200;
+const int MAX_ANSWER_AMOUNT = 50;
+const int MAX_LIKERT_AMOUNT = 10;
+
+
 int* getControlBits() {
     static int control_bits[4];
-    char line[3000];
+    char line[MAX_LINE_LENGTH];
     char *token;
     int line_number = 0;
 
-    while(fgets(line, 3000, stdin) != (NULL)){
+    while(fgets(line, MAX_LINE_LENGTH, stdin) != (NULL)){
         line_number++; //for debug purposes. 
         //if the line starts with a #, skip it. 
         if (line[0] == '#'){
@@ -34,12 +40,10 @@ int* getControlBits() {
 
 
 void getQuestions(char *arr[]) {
-    char line[3000];
+    char line[MAX_LINE_LENGTH];
     char *token;
-    int line_number = 0;
 
-    while(fgets(line, 3000, stdin) != (NULL)){
-        line_number++; //for debug purposes. 
+    while(fgets(line, MAX_LINE_LENGTH, stdin) != (NULL)){
         //if the line starts with a #, skip it. 
         if (line[0] == '#'){
             continue; 
@@ -47,16 +51,16 @@ void getQuestions(char *arr[]) {
         token = strtok(line, ";");
         int i = 0;
         while (token != NULL) {
-            //malloc space for the question
+            //malloc space for the question (epic casts for the win)
             arr[i] = (char*)malloc(strlen(token) + 1);
             //copy the question into the array
-            strcpy(arr[i], token);
+            strncpy(arr[i], token, strlen(token));
             //get the next token
             token = strtok(NULL, ";");
             i++;
         } 
         //make the rest of the questions array NULL
-        for (int j = i; j < 200; j++) {
+        for (int j = i; j < MAX_QUESTION_AMOUNT; j++) {
             arr[j] = NULL;
         }
 
@@ -66,25 +70,101 @@ void getQuestions(char *arr[]) {
 
 }
 
+void getAnswerOptions(char* arr[]){
+    char line[MAX_LINE_LENGTH];
+    char *token;
+
+    //get the answers. 
+    while(fgets(line, MAX_LINE_LENGTH, stdin) != (NULL)){
+        //if the line starts with a #, skip it. 
+        if (line[0] == '#'){
+            continue; 
+        }
+        token = strtok(line, ";");
+        int i = 0;
+        while (token != NULL) {
+            //malloc space for the question (epic casts for the win)
+            arr[i] = (char*)malloc(strlen(token) + 1);
+            //copy the question into the array
+            strncpy(arr[i], token, strlen(token));
+            //get the next token
+            token = strtok(NULL, ";");
+            i++;
+        } 
+        //make the rest of the questions array NULL
+        for (int j = i; j < MAX_ANSWER_AMOUNT; j++) {
+            arr[j] = NULL;
+        }
+
+        break; //we only want the first line of questions.
+    }
+}
+
+void getLikertOptions(char* arr[]){
+    char line[MAX_LINE_LENGTH];
+    char *token;
+
+    //get the answers. 
+    while(fgets(line, MAX_LINE_LENGTH, stdin) != (NULL)){
+        //if the line starts with a #, skip it. 
+        if (line[0] == '#'){
+            continue; 
+        }
+        token = strtok(line, ",");
+        int i = 0;
+        while (token != NULL) {
+            //malloc space for the question (epic casts for the win)
+            arr[i] = (char*)malloc(strlen(token) + 1);
+            //copy the question into the array
+            strncpy(arr[i], token, strlen(token));
+            //get the next token
+            token = strtok(NULL, ",");
+            i++;
+        } 
+        //make the rest of the questions array NULL
+        for (int j = i; j < MAX_LIKERT_AMOUNT; j++) {
+            arr[j] = NULL;
+        }
+
+        break; //we only want the first line of questions.
+    }
+}
+
 
 int main() {
-    const int MAX_QUESTION_LENGTH = 3000;
-    char* questions[200];
-
+    char* questions[MAX_QUESTION_AMOUNT];
+    char* answer_options[MAX_ANSWER_AMOUNT];
+    char* likert_options[MAX_LIKERT_AMOUNT];
     int* control_bits = getControlBits();
-    
+
     getQuestions(questions); 
+    getAnswerOptions(answer_options);
+    getLikertOptions(likert_options);
     // Print control bits for debugging
     printf("Control Bits: \n");
     for(int i = 0; i < 4; i++) {
         printf("  Bit %d: %d\n", i, control_bits[i]);
     }
     // Print questions for debugging
-   for (int i = 0; i < 200; i++) {
+   for (int i = 0; i < MAX_QUESTION_AMOUNT; i++) {
         if (questions[i] == NULL) {
             break;
         }
         printf("Question %d: %s\n", i, questions[i]);
+    }
+    // Print answer options for debugging
+    for (int i = 0; i < MAX_ANSWER_AMOUNT; i++) {
+        if (answer_options[i] == NULL) {
+            break;
+        }
+        printf("Answer %d: %s\n", i, answer_options[i]);
+    }
+    // Print likert options for debugging
+    for (int i = 0; i < MAX_LIKERT_AMOUNT; i++) {
+        if (likert_options[i] == NULL) {
+            break;
+        }
+        printf("Likert %d: %s\n", i, likert_options[i]);
     }
     return 0;
 }
