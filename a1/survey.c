a291afd79fc65503108246ef540c9a77900d5168
Isaac Preyser <preyser@uvic.ca>
2024-09-21 17:47:56 -0700

    refactored code; made code slightly more efficient.

--- a1/survey.c
+++ a1/survey.c
@@ -30,134 +30,90 @@ typedef struct{
 }Response;
 
 int* getControlBits() {
-    static int control_bits[4];
+    static int control_bits[4] = {0};  // initialize to zero
     char line[MAX_LINE_LENGTH];
-    char *token;
-    int line_number = 0;
 
-    while(fgets(line, MAX_LINE_LENGTH, stdin) != (NULL)){
-        line_number++; //for debug purposes. 
-        //if the line starts with a #, skip it. 
-        if (line[0] == '#'){
-            continue; 
+    while (fgets(line, MAX_LINE_LENGTH, stdin) != NULL) {
+        // skip comment lines
+        if (line[0] == '#') {
+            continue;
         }
-        token = strtok(line, ",\n");
-        int i = 0;
-        while (token != NULL && i < 4) {
-            control_bits[i] = atoi(token);
-            token = strtok(NULL, ",\n");
-            i++;
+
+        // try to parse 4 integers from the line
+        int parsed = sscanf(line, "%d,%d,%d,%d", 
+                            &control_bits[0], &control_bits[1], 
+                            &control_bits[2], &control_bits[3]);
+
+        // if we parsed at least one integer, we're done
+        if (parsed > 0) {
+            break;
         }
-        break; 
+        // if we couldn't parse any integers, continue to the next line
     }
 
     return control_bits;
 }
 
 
+void getOptions(char *arr[], int max_size, const char *delimiters) {
+    char line[MAX_LINE_LENGTH];
+    char *token;
+
+    while (fgets(line, MAX_LINE_LENGTH, stdin) != NULL) {
+        if (line[0] == '#') {
+            continue;
+        }
+        
+        int i = 0;
+        token = strtok(line, delimiters);
+        while (token != NULL && i < max_size) {
+            size_t token_len = strlen(token); //slightly smaller than an int, and avoids recomputing the length of the token.
+            arr[i] = malloc(token_len + 1);
+            if (arr[i] == NULL) {
+                fprintf(stderr, "Memory allocation failed\n");
+                exit(1);
+            }
+            strncpy(arr[i], token, token_len);
+            arr[i][token_len] = '\0';  // ensure null-termination
+            token = strtok(NULL, delimiters);
+            i++;
+        }
+
+        // null-terminate the rest of the array
+        for (int j = i; j < max_size; j++) {
+            arr[i] = NULL;
+        }
+
+        break;  // we only want the first non-comment line
+    }
+}
+
+//these really don't need to be separate functions, but I'm doing it for the sake of readability.
 void getQuestions(char *arr[]) {
-    char line[MAX_LINE_LENGTH];
-    char *token;
-
-    while(fgets(line, MAX_LINE_LENGTH, stdin) != (NULL)){
-        //if the line starts with a #, skip it. 
-        if (line[0] == '#'){
-            continue; 
-        }
-        token = strtok(line, ";\n");
-        int i = 0;
-        while (token != NULL) {
-            //malloc space for the question (epic casts for the win)
-            arr[i] = (char*)malloc(strlen(token) + 1);
-            //copy the question into the array
-            strncpy(arr[i], token, strlen(token));
-            //get the next token
-            token = strtok(NULL, ";\n");
-            i++;
-        } 
-        //make the rest of the questions array NULL
-        for (int j = i; j < MAX_QUESTION_AMOUNT; j++) {
-            arr[j] = NULL;
-        }
-
-        break; //we only want the first line of questions.
-    }
-    return;
-
+    getOptions(arr, MAX_QUESTION_AMOUNT, ";\n");
 }
 
-void getAnswerOptions(char* arr[]){
-    char line[MAX_LINE_LENGTH];
-    char *token;
-
-    //get the answers. 
-    while(fgets(line, MAX_LINE_LENGTH, stdin) != (NULL)){
-        //if the line starts with a #, skip it. 
-        if (line[0] == '#'){
-            continue; 
-        }
-        token = strtok(line, ";\n");
-        int i = 0;
-        while (token != NULL) {
-            //malloc space for the question (epic casts for the win)
-            arr[i] = (char*)malloc(strlen(token) + 1);
-            //copy the question into the array
-            strncpy(arr[i], token, strlen(token));
-            //get the next token
-            token = strtok(NULL, ";\n");
-            i++;
-        } 
-        //make the rest of the questions array NULL
-        for (int j = i; j < MAX_ANSWER_AMOUNT; j++) {
-            arr[j] = NULL;
-        }
-
-        break; //we only want the first line of questions.
-    }
+void getAnswerOptions(char *arr[]) {
+    getOptions(arr, MAX_ANSWER_AMOUNT, ";\n");
 }
 
-void getLikertOptions(char* arr[]){
-    char line[MAX_LINE_LENGTH];
-    char *token;
-
-    //get the answers. 
-    while(fgets(line, MAX_LINE_LENGTH, stdin) != (NULL)){
-        //if the line starts with a #, skip it. 
-        if (line[0] == '#'){
-            continue; 
-        }
-        token = strtok(line, ",\n");
-        int i = 0;
-        while (token != NULL) {
-            //malloc space for the question (epic casts for the win)
-            arr[i] = (char*)malloc(strlen(token) + 1);
-            //copy the question into the array
-            strncpy(arr[i], token, strlen(token));
-            //get the next token
-            token = strtok(NULL, ",\n");
-            i++;
-        } 
-        //make the rest of the questions array NULL
-        for (int j = i; j < MAX_LIKERT_AMOUNT; j++) {
-            arr[j] = NULL;
-        }
-
-        break; //we only want the first line of questions.
-    }
+void getLikertOptions(char *arr[]) {
+    getOptions(arr, MAX_LIKERT_AMOUNT, ",\n");
 }
 
 Question constructQuestion(char* question, char* answer_option){
     Question q;
     q.question_type = question[0];
     q.question_number = atoi(&question[1]); //this is a hack lmao
+    int len = strlen(question);
     //check that the question is longer than 4 characters. 
-    if (strlen(question) < 4){
+    if (len < 4){
         printf("Error: Question is too short. Exiting.\n");
         exit(1);
     }
-    q.question = malloc(strlen(question) - 3);
-    strncpy(q.question, &question[4], strlen(question) - 4); //we just want the question content. 
-    q.question[strlen(question) - 4] = '\0'; //null terminate the string.
+    q.question = malloc(len - 3);
+    strncpy(q.question, &question[4], len - 4); //we just want the question content. 
+    q.question[len - 4] = '\0'; //null terminate the string.
     q.is_reverse = 0; //default to false.
     if (answer_option[0] == 'R'){
         q.is_reverse = 1;
@@ -165,72 +121,84 @@ Question constructQuestion(char* question, char* answer_option){
     return q;
 }
 
-Response getNextResponse(){
-    Response r;
+Response getNextResponse() {
+    Response r = {0};  // zero-initialize all fields
     char line[MAX_LINE_LENGTH];
     char *token;
 
-    // Initialize the response with null values
-    r.major = NULL;
-    r.yes_no = -1;
-    r.dob_year = -1;
-    r.dob_month = -1;
-    r.dob_day = -1;
-    for (int i = 0; i < MAX_QUESTION_AMOUNT; i++) {
-        r.answer[i] = NULL;
+    while (fgets(line, MAX_LINE_LENGTH, stdin) != NULL) {
+        if (line[0] == '#') continue;  // skip comment lines
+        break;
     }
 
-
-    while(fgets(line, MAX_LINE_LENGTH, stdin) != (NULL)){
-        //if the line starts with a #, skip it. 
-        if (line[0] == '#'){
-            continue; 
-        }
-        //otherwise, we are reading a response.
-        break; 
-    }
-    //check if the line is empty. if it is, we are done reading responses.
-     if (line[0] == '\0' || feof(stdin)) {
-        return r; // Return the "empty" response
+    if (line[0] == '\0' || feof(stdin)) {
+        return r;  // return the "empty" response if no more input
     }
 
-
-    //we need to tokenize the line to grab the appropriate data.
+    // major
     token = strtok(line, ",\n");
-    r.major = malloc(strlen(token) + 1);
-    strncpy(r.major, token, strlen(token));
-    r.major[strlen(token)] = '\0'; //null terminate the string.
-    token = strtok(NULL, ",\n");
-    if (strcmp(token, "yes") == 0){
-        r.yes_no = 1;
-    } else {
-        r.yes_no = 0;
+    if (token) {
+        r.major = malloc(strlen(token) + 1);
+        strncpy(r.major, token, strlen(token));
+        r.major[strlen(token)] = '\0';  // ensure null-termination
     }
+
+    // yes/no
     token = strtok(NULL, ",\n");
-    // Parse the date of birth using sscanf
-    sscanf(token, "%d-%d-%d", &r.dob_year, &r.dob_month, &r.dob_day);
-    printf("Debug: Read DOB: %d-%d-%d\n", r.dob_year, r.dob_month, r.dob_day);
-    // Read the answers
+    if (token) r.yes_no = (strcmp(token, "yes") == 0) ? 1 : 0;
+
+    // date of birth
+    token = strtok(NULL, ",\n");
+    if (token) {
+        sscanf(token, "%d-%d-%d", &r.dob_year, &r.dob_month, &r.dob_day);
+    }
+
+    // answers
     for (int i = 0; i < MAX_QUESTION_AMOUNT; i++) {
         token = strtok(NULL, ",\n");
-        if (token == NULL){
-            printf("Debug: No more answers, breaking at i=%d\n", i);
-            break; 
-        }
+        if (!token) break;
         r.answer[i] = malloc(strlen(token) + 1);
         strncpy(r.answer[i], token, strlen(token));
-        r.answer[i][strlen(token)] = '\0'; //null terminate the string.
-        printf("Debug: Read answer[%d]: %s\n", i, r.answer[i]);
+        r.answer[i][strlen(token)] = '\0';  // ensure null-termination
     }
 
     return r;
-
 }
 
 int scoreResponse(){
     
     
     return 0;
+}
+
+void freeStringArray(char** arr, int max_size) {
+    for (int i = 0; i < max_size; i++) {
+        if (arr[i] == NULL) {
+            break;
+        }
+        free(arr[i]);
+    }
+}
+
+void freeResponses(Response* responses, int response_count) {
+    for (int i = 0; i < response_count; i++) {
+        if (responses[i].major != NULL) {
+            free(responses[i].major);
+        }
+        for (int j = 0; j < MAX_QUESTION_AMOUNT; j++) {
+            if (responses[i].answer[j] != NULL) {
+                free(responses[i].answer[j]);
+            }
+        }
+    }
+}
+
+void cleanup(char** questions, char** answer_options, char** likert_options, 
+             Response* responses, int response_count) {
+    freeStringArray(questions, MAX_QUESTION_AMOUNT);
+    freeStringArray(answer_options, MAX_ANSWER_AMOUNT);
+    freeStringArray(likert_options, MAX_LIKERT_AMOUNT);
+    freeResponses(responses, response_count);
 }
 
 int main() {
@@ -243,7 +211,6 @@ int main() {
     getAnswerOptions(answer_options);
     getLikertOptions(likert_options);
 
-    // Commented out debugging code...
 
     // Construct the questions
     Question q[MAX_QUESTION_AMOUNT];
@@ -254,59 +221,41 @@ int main() {
         q[i] = constructQuestion(questions[i], answer_options[i]);
     }
 
+    // // print out the questions for debugging purposes.
+    // for (int i = 0; i < MAX_QUESTION_AMOUNT; i++){
+    //     if (q[i].question == NULL){
+    //         break;
+    //     }
+    //     printf("Question %c%d: %s\n", q[i].question_type, q[i].question_number, q[i].question);
+    //     printf("Reverse: %d\n", q[i].is_reverse);
+    // }
+
     // Get responses (1 per line until EOF)
     Response responses[MAX_QUESTION_AMOUNT];
     int response_count = 0;
     for (int i = 0; i < MAX_QUESTION_AMOUNT; i++) {
         responses[i] = getNextResponse();
         if (responses[i].major == NULL) {
-            // We've reached the end of the input
+            // no more responses
             response_count = i;
             break;
         }
     }
-    //print out the responses for debugging purposes.
-    for (int i = 0; i < response_count; i++){
-        printf("Major: %s\n", responses[i].major);
-        printf("Yes/No: %d\n", responses[i].yes_no);
-        printf("DOB: %d-%d-%d\n", responses[i].dob_year, responses[i].dob_month, responses[i].dob_day);
-        for (int j = 0; j < MAX_QUESTION_AMOUNT; j++){
-            if (responses[i].answer[j] == NULL){
-                break; 
-            }
-            printf("Answer %d: %s\n", j, responses[i].answer[j]);
-        }
-    }
+    // // print out the responses for debugging purposes.
+    // for (int i = 0; i < response_count; i++){
+    //     printf("Major: %s\n", responses[i].major);
+    //     printf("Yes/No: %d\n", responses[i].yes_no);
+    //     printf("DOB: %d-%d-%d\n", responses[i].dob_year, responses[i].dob_month, responses[i].dob_day);
+    //     for (int j = 0; j < MAX_QUESTION_AMOUNT; j++){
+    //         if (responses[i].answer[j] == NULL){
+    //             break; 
+    //         }
+    //         printf("Answer %d: %s\n", j, responses[i].answer[j]);
+    //     }
+    // }
 
-    // Before we exit, we MUST free the memory
-    for (int i = 0; i < MAX_QUESTION_AMOUNT; i++) {
-        if (questions[i] == NULL) {
-            break;
-        }
-        free(questions[i]);
-    }
-    for (int i = 0; i < MAX_ANSWER_AMOUNT; i++) {
-        if (answer_options[i] == NULL) {
-            break;
-        }
-        free(answer_options[i]);
-    }
-    for (int i = 0; i < MAX_LIKERT_AMOUNT; i++) {
-        if (likert_options[i] == NULL) {
-            break;
-        }
-        free(likert_options[i]);
-    }
-    for (int i = 0; i < response_count; i++) {
-        if (responses[i].major != NULL) {
-            free(responses[i].major);
-        }
-        for (int j = 0; j < MAX_QUESTION_AMOUNT; j++) {
-            if (responses[i].answer[j] != NULL) {
-                free(responses[i].answer[j]);
-            }
-        }
-    }
+    // before we exit, we MUST free the memory
+    cleanup(questions, answer_options, likert_options, responses, response_count);
     
     return 0;
 }
